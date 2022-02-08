de0d354b529b9e05bda0f7c738598851b3dd8258
Alex Shilenkov <85848281+Alex-esc1@users.noreply.github.com>
2022-02-08 15:27:08 +0300

    AppBar задал белого цвета

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,7 @@ class MyApp extends StatelessWidget {
             style: TextStyle(color: Colors.black),
           ),
           centerTitle: true,
+          backgroundColor: Colors.white,
         ),
         body: null,
       ),
